72e46de4800e24bbf7add33eb2cc60cb8106e26e
Danila1080 <109276687+Danila1080@users.noreply.github.com>
2023-12-06 23:16:28 +0300

    Добавил макет главного меню

--- menu.py
+++ menu.py
@@ -30,3 +30,44 @@ class Button:
         if self.rect.collidepoint(mouse_pos):
             return True
         return False
+
+
+# главное меню
+def main_menu(screen):
+    start_button = Button('Играть', (250, 150), 150, 50, 'green', 'red', 'purple', 40)
+    settings_button = Button('Настройки', (250, 210), 150, 50, 'green', 'red', 'purple', 40)
+    exit_button = Button('Выход', (250, 270), 150, 50, 'green', 'red', 'purple', 40)
+
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+        main_background = pygame.image.load('Resources\Images\dream_TradingCard.jpg')
+        main_background = pygame.transform.scale(main_background, (640, 480))
+        screen.blit(main_background, (0, 0))
+        start_button.draw(screen)
+        settings_button.draw(screen)
+        exit_button.draw(screen)
+
+        font = pygame.font.Font(None, 80)
+        text_surface = font.render("Побег", True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=(320, 100))
+        screen.blit(text_surface, text_rect)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if start_button.is_hovered(event.pos):
+                    # start
+                    pass
+                elif exit_button.is_hovered(event.pos):
+                    running = False
+                elif settings_button.is_hovered(event.pos):
+                    pass
+        pygame.display.flip()
+
+
+# pygame.init()
+# size = width, height = 640, 480
+# screen = pygame.display.set_mode(size)
+# main_menu(screen)
