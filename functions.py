8c32ac927198565ba2e9e873261f27e46a4ef00d
KOT05 <tida050708@mail.ru>
2024-01-19 01:16:57 +0300

    Финальная версия

--- functions.py
+++ functions.py
@@ -2,37 +2,6 @@ import pygame
 from time import time
 
 
-# класс кнопки
-class Button:
-    def __init__(self, text, position, width, height, color, hover_color, text_color, font_size):
-        self.text = text
-        self.position = position
-        self.width = width
-        self.height = height
-        self.color = color
-        self.hover_color = hover_color  # Цвет при наведении
-        self.text_color = text_color
-        self.font_size = font_size  # Размер шрифта
-        self.font = pygame.font.Font(None, self.font_size)  # шрифт по умолчанию указанного размера
-        self.rect = pygame.Rect(self.position[0], self.position[1], self.width, self.height)
-        self.hovered = False
-
-    def draw(self, screen):
-        if self.is_hovered(pygame.mouse.get_pos()):
-            pygame.draw.rect(screen, self.hover_color, self.rect)
-        else:
-            pygame.draw.rect(screen, self.color, self.rect)
-
-        text_surface = self.font.render(self.text, True, self.text_color)
-        text_rect = text_surface.get_rect(center=self.rect.center)
-        screen.blit(text_surface, text_rect)
-
-    def is_hovered(self, mouse_pos):  # проверка наведения мыши на кнопку
-        if self.rect.collidepoint(mouse_pos):
-            return True
-        return False
-
-
 # класс музыкального проигрывателя
 class SoundPlayer:
     def __init__(self):
@@ -56,13 +25,6 @@ class SoundPlayer:
 
     def set_volume(self, volume):
         pygame.mixer.music.set_volume(volume)
-
-
-def rendering(screen, way_to_picture, picture_size):
-    screen.fill((0, 0, 0))
-    background = pygame.image.load(way_to_picture)
-    background = pygame.transform.scale(background, picture_size)
-    screen.blit(background, (0, 0))
 
 
 def render_of_text(screen, text, rect_center, size=80, color='gray'):
